c99a96810acc9f94c7a4d39e161eaab83a7da524
oigreslol <sergiorojas12ocm@gmail.com>
2020-06-19 21:03:18 -0500

    Update method Requests route

--- src/routes/requests.routes.js
+++ src/routes/requests.routes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/:celebrityid', async (req, res) => {
-    var { message, delivered, buyerid } = req.body
+    var { message, delivered, buyerid } = req.body;
     //the buyer needs to be the current user auth
     //------- get user auth logic here ----
 
@@ -26,6 +26,37 @@ router.post('/:celebrityid', async (req, res) => {
     //for this step I only save the celebrityid into the field celebrity of requests model
     var request = new Requests({buyer:buyerid,celebrity:celebrityid,delivered:delivered,message:message});
     await request.save();
+    res.json({
+        statusCode:200,
+        body: "The request was correctly save"
+    });
+});
+
+function CheckFields(body,fields){
+    var checkedObject = {};
+    fields
+        .filter(element => !(body[element] === undefined || body[element] === null))
+        .forEach(element => {
+            checkedObject[element] =  body[element];
+        })
+    return checkedObject;
+}
+
+//middlewareObjectDoesntExist = function
+
+
+router.put('/:requestsid', async (req, res, next) => {
+    //try if is an invalid request with simbol strange
+    var request_id = req.params.requestsid;
+    //add try to catch if is null
+    var requests = await Requests.findById(request_id, function (err, tickets) {
+        if (err) return next(err);
+    });
+
+    var necesaryFields = ["message","delivered"];
+    var updater = CheckFields(req.body,necesaryFields);
+    await requests.update(updater);
+    
     res.json({
         statusCode:200,
         body: "The request was correctly save"
