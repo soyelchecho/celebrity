c9ab1b6120e12c3c381637e3d5992cd2f6fb1e95
oigreslol <sergiorojas12ocm@gmail.com>
2020-06-26 17:39:46 -0500

    Component Comprenhension

--- celebrity/src/components/Request.jsx
+++ celebrity/src/components/Request.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
-import {useDispatch, useSelector} from 'react-redux'
-import {getRequestAction, nextRequestAction} from '../redux/requestDucks'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getRequestAction, nextRequestAction } from "../redux/requestDucks";
 
+const List = (props) => {
+  return (
+    <ul>
+      {props.requests !== undefined ? props.requests.map((item) => (
+        <li key={item._id}>{item.message}</li>
+      )) : <h1>Cargando...</h1>}
+    </ul>
+  );
+};
 
 const Request = () => {
-    
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRequestAction());
+  }, []);
 
-    const requests = useSelector(store => store.requests.array);
+  const requests = useSelector((store) => store.requests.array);
 
-    return (
-        <div>
-            Requets List:
-            <button onClick={() => dispatch(getRequestAction())}>Get All Requests</button>
-            <button onClick={() => dispatch(nextRequestAction())}>Next</button>
-            <ul>
-                {
-                    requests.map(item =>(
-                        <li key={item._id}>{item.message}</li>
-                    ))
-                }
-            </ul>
-        </div>
-    );
-}
- 
+  return (
+    <div>
+      Requets List:
+      <button onClick={() => dispatch(nextRequestAction())}>Next</button>
+      {<List requests={requests} />}
+    </div>
+  );
+};
+
 export default Request;
